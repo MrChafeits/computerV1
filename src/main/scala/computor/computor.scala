6c23f075d927fbc7694e688f565bf2f7c52924a0
Clayton Allen <mrchafeits@aaathats3as.com>
2020-02-05 19:40:05 -0800

    feat: implemented Polynomial simplify and solve

--- src/main/scala/computor/computor.scala
+++ src/main/scala/computor/computor.scala
@@ -2,22 +2,49 @@ package computor
 
 import scala.util.{Try, Success, Failure}
 import scala.math.pow
+import scala.collection._
 
 /*
- * variables
- * operators
- * expression
- * equals
- * polynomial
+ * input string: "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"
+ * variables: X
+ * operators: + -
+ * expression: ???
+ * equals: lhs = rhs
+ * polynomial: lhs["5 * X^0", "4 * X^1", "-9.3 * X^2"] equals rhs["1 * X^0"]
+ * lhs[Term(5.0, 0), Term(4.0, 1), Term(-9.3, 2)] equals rhs[Term(1, 0)]
+ */
+/* A parser for things
+ * Is a function from strings
+ * To lists of pairs
+ * Of things and strings
+ */
+/* Basic Minimum Grammar
+ * %%
+ * Expr          ::= Term "+" Expr | Term "-" Expr | Term
+ * Term          ::= Coeff " * " Indeterminate "^" Exponent
+ * Coeff         ::= Number
+ * Exponent      ::= Integer
+ * Indeterminate ::= "X"
+ * Number        ::= a
  */
 
-case class Term(coefficient: Double, exponent: Int)
-
-class Parser(input: String) {
-  def toPolynomial(): Try[Polynomial] = ???
+case class Term(coefficient: Double, exponent: Int) {
+  // TODO: throw error for trying to add terms of different exponents
+  def +(rhs: Term): Term =
+    Term(coefficient + rhs.coefficient, exponent)
+  def unary_-(): Term = Term(-coefficient, exponent)
 }
 
-class Quadratic(a: Double, b: Double, c: Double) {
+case class Parser(input: String) {
+  def toPolynomial(): Try[Polynomial] = {
+    Success(
+      Polynomial(Seq(Term(5.0,0), Term(4.0,1), Term(-9.3,2)),
+      Seq(Term(1.0, 0)))
+    )
+  }
+}
+
+case class Quadratic(a: Double, b: Double, c: Double) {
   def solve(): (Option[Double], Option[Double]) = {
     val + = (a:Double, b:Double) => a + b
     val - = (a:Double, b:Double) => a - b
@@ -37,7 +64,26 @@ class Quadratic(a: Double, b: Double, c: Double) {
   }
 }
 
-class Polynomial(lhs: List[Term], rhs: List[Term]) {
+case class Polynomial(lhs: Seq[Term], rhs: Seq[Term]) {
+  def combine(x: (Term, Term, Term), y: Term) = {
+    y.exponent match {
+      case 0 => (x._1 + y, x._2, x._3)
+      case 1 => (x._1, x._2 + y, x._3)
+      case 2 => (x._1, x._2, x._3 + y)
+    }
+  }
+  def simplify(): Polynomial = {
+    val newleft = (lhs ++ rhs.map((t: Term) => -t))
+    val l = newleft.foldLeft((Term(0, 0), Term(0, 1), Term(0, 2)))(combine)
+    Polynomial(Seq(l._1, l._2, l._3), Seq())
+
+  }
   // Reduce polynomial and send to Quadratic to solve
-  def solve(): (Option[Double], Option[Double]) = ???
+  def solve(): (Option[Double], Option[Double]) = {
+    val s = simplify
+    val v = s.lhs.reverse
+    val r = v.map(_.coefficient)
+    Quadratic(r(0), r(1), r(2)).solve
+    // Quadratic(s.lhs.reverse.map(_.coefficient)).solve
+  }
 }
--- src/test/scala/SetSuite.scala
+++ src/test/scala/SetSuite.scala
@@ -1,8 +1,22 @@
 import org.scalatest.FunSuite
 import computor._
 
+/*
+5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0
+5 * X^0 + 4 * X^1 = 4 * X^0
+8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0
+5 + 4 * X + X^2= X^2
+1+4*X-X^2=0
+9x^2+6x+3
+*/
+
 class SetSuite extends FunSuite {
   test("Correct Calculations") {
     assert(new Quadratic(2, 5, -3).solve == (Some(0.5), Some(-3.0)))
+  }
+  test("Poltnomial") {
+    val l = Seq(Term(3, 0), Term(6, 1), Term(3, 2))
+    val r = Seq(Term(0, 0), Term(0, 1), Term(0, 2))
+    println(Polynomial(l, r).simplify.solve)
   }
 }
