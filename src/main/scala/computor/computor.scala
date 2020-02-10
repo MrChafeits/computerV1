25e2b03dd3acab3180da4fe5c663f452f39c23cd
Clayton Allen <mrchafeits@aaathats3as.com>
2020-02-09 20:52:51 -0800

    fix: simplify polynomials of degree > 2

--- src/main/scala/computor/computor.scala
+++ src/main/scala/computor/computor.scala
@@ -1,10 +1,11 @@
 package computor
 
 import scala.util.{Try, Success, Failure}
-import scala.math.pow
+import scala.math.{pow, Ordered}
+import scala.util.Sorting
 import scala.collection._
 import scala.util.parsing.combinator._
-import scala.util.parsing.input.Positional
+import scala.util.parsing.input.{Position, Positional}
 
 /*
  * input string: "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"
@@ -42,22 +43,34 @@ import scala.util.parsing.input.Positional
  * Number   ::= a
  */
 
-case class Term(coefficient: Double, exponent: Int) {
+case class Term(coefficient: Double, exponent: Int) extends Ordered[Term] {
+  def compare(that: Term) = this.exponent - that.exponent
   // TODO: throw error for trying to add terms of different exponents
   def +(rhs: Term): Term =
     Term(coefficient + rhs.coefficient, exponent)
   def unary_-(): Term = Term(-coefficient, exponent)
+  def longString = coefficient+" * X^"+exponent
 }
 
 // TODO: move parser to separate file
-class PolynomialLexer extends JavaTokenParsers {
+object PolynomialLexer extends JavaTokenParsers {
   override def skipWhitespace = true
+
+  def apply(input: String): Either[NoSuccess, Polynomial] = parseAll(polynomial, input) match {
+    case failure : NoSuccess => Left(failure)
+    case Success(result, _) => Right(result)
+  }
   def polynomial: Parser[Polynomial] = {
     ((expression) ~ "=" ~ (expression)) ^^ {
       case lhs ~ equ ~ rhs => Polynomial(lhs, rhs)
     }
   }
   def expression: Parser[List[Term]] = (rep1(term))
+  def term: Parser[Term] = {
+    ((coefficient) ~ "*" ~ (exponent)) ^^ {
+      case c ~ op ~ p => Term(c, p)
+    }
+  }
   def coefficient: Parser[Double] = {
     (opt("[+-]".r) ~ decimalNumber) ^^ {
       case Some(op) ~ num => {
@@ -75,17 +88,7 @@ class PolynomialLexer extends JavaTokenParsers {
       case v ~ e ~ n => n.toInt
     }
   }
-  def term: Parser[Term] = {
-    ((coefficient) ~ "*" ~ (exponent)) ^^ {
-      case c ~ op ~ p => Term(c, p)
-    }
-  }
   def variable: Parser[String] = "[A-Za-z]".r
-  // def toPolynomial(s: String): Try[Polynomial] = {
-  //   apply(s) match {
-  //     case (lhs, rhs) => Success(Polynomial(lhs, rhs))
-  //   }
-  // }
 }
 
 case class Quadratic(a: Double, b: Double, c: Double) {
@@ -103,24 +106,34 @@ case class Quadratic(a: Double, b: Double, c: Double) {
 }
 
 case class Polynomial(lhs: Seq[Term], rhs: Seq[Term]) {
-  def combine(x: (Term, Term, Term), y: Term) = {
-    y.exponent match {
-      case 0 => (x._1 + y, x._2, x._3)
-      case 1 => (x._1, x._2 + y, x._3)
-      case 2 => (x._1, x._2, x._3 + y)
-    }
+  def degree(): Int = (lhs.isEmpty, rhs.isEmpty) match {
+    case (false, false) => Seq(lhs.max, rhs.max).max.exponent
+    case (true, false) => rhs.max.exponent
+    case (false, true) => lhs.max.exponent
+    case _ => 0
   }
   def simplify(): Polynomial = {
     val newleft = (lhs ++ rhs.map((t: Term) => -t))
-    val l = newleft.foldLeft((Term(0, 0), Term(0, 1), Term(0, 2)))(combine)
-    Polynomial(Seq(l._1, l._2, l._3), Seq())
+    Polynomial(newleft.groupBy((t: Term) => t.exponent)
+      .map({ case (k, v) => (k, v.reduceLeft(_ + _)) })
+      .values.toList.sorted, Seq())
   }
   // Reduce polynomial and send to Quadratic to solve
   def solve(): (Option[Double], Option[Double]) = {
     val s = simplify
     val v = s.lhs.reverse
     val r = v.map(_.coefficient)
-    Quadratic(r(0), r(1), r(2)).solve
-    // Quadratic(s.lhs.reverse.map(_.coefficient)).solve
+    degree match {
+      case 0 => (Some(r(0)), None)
+      case 1 => (Some(-r(1) / r(0)), None)
+      case 2 => Quadratic(r(0), r(1), r(2)).solve
+      case n => ??? // Failure(s"Expected degree < 3, found $n")
+    }
   }
+
+  def exprString(expr: Seq[Term]) = expr.isEmpty match {
+    case false => expr.head.longString+expr.tail.map{x => " + "+x.longString}.mkString
+    case true => "0"
+  }
+  def longString = exprString(lhs)+" = "+exprString(rhs)
 }
--- src/test/scala/SetSuite.scala
+++ src/test/scala/SetSuite.scala
@@ -1,51 +1,79 @@
 import org.scalatest.funsuite.AnyFunSuite
 import computor._
+import scala.math.sqrt
 
 /*
-5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0
-5 * X^0 + 4 * X^1 = 4 * X^0
-8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0
-5 + 4 * X + X^2= X^2
+equation: 5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0
+reduced:  4 * X^0 + 4 * X^1 - 9.3 * X^2 = 0
+degree: 2
+Discriminant is strictly positive, the two solutions are:
+0.905239
+-0.475131
+
+equation: 5 * X^0 + 4 * X^1 = 4 * X^0
+reduced:  1 * X^0 + 4 * X^1 = 0
+degree: 1
+The solution is:
+-0.25
+
+equation: 8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0
+reduced:  5 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 0
+degree: 3
+Degree > 2, cannot solve
+=== Bonus ===
+equation: 5 + 4 * X + X^2= X^2
+reduced:  5 + 4 * x = 0
+degree: 1
+The solution is:
+-1.25
+
 1+4*X-X^2=0
 9x^2+6x+3
 */
 
 
 class SetSuite extends AnyFunSuite {
+  test("TermOrdering") {
+    assert(Term(1, 0) < Term(-1, 1))
+
+    val arr = Array(Term(0, 2), Term(0, 1), Term(0, 0))
+    assert(arr.max == Term(0, 2))
+  }
   test("Correct Calculations") {
     assert(new Quadratic(2, 5, -3).solve == (Some(0.5), Some(-3.0)))
   }
   test("Poltnomial") {
-    val l = Seq(Term(3, 0), Term(6, 1), Term(3, 2))
-    val r = Seq(Term(0, 0), Term(0, 1), Term(0, 2))
-    println(Polynomial(l, r).simplify.solve)
+    val l = Seq(Term(1, 0), Term(6, 1), Term(0, 2))
+    val r = Seq(Term(-1, 0), Term(-2, 2), Term(-1, 0), Term(0, 1), Term(-1, 2))
+    assert(Polynomial(l, r).solve == (Some(-1.0), None))
+
+    val l1 = Seq(Term(-1, 2), Term(1, 0), Term(5, 2), Term(0, 1))
+    val r1 = Seq(Term(9, 3), Term(6, 2), Term(7, 1))
+    val p = Polynomial(l1, r1)
+    println(p.simplify.longString)
   }
-  // test("Parser") {
-  //   case class TermTest(coeff: Double, exponent: Int)
-  //   sealed trait TermToken extends Positional
-  //   case class NUMBER(str: String) extends TermToken
-  //   case class ADDTERM(str: String) extends TermToken
-  //   case class SUBTERM(str: String) extends TermToken
-  //   case class NEGTERM(str: String) extends TermToken
-  //   object PolynomialLexer extends JavaTokenParsers {
-  //     override def skipWhitespace = true
-  //     def number: Parser[Double] = positioned { floatingPointNumber ^^ { str => NUMBER(str) } }
-  //     def poly: Parser[POLY] = positioned { expr ~ opt("=" ~ expr) ^^ { str => POLY(str) } }
-  //     def expr: Parser[EXPR] = positioned { term ~ rep("+" ~ term | "-" ~ term) ^^ { str => EXPR(str) } }
-  //     def term: Parser[TERM] = positioned { coef ~ "*" ~ varb ^^ { str => TERM(str) } }
-  //     def varb: Parser[VARB] = positioned { "X^" ~ wholeNumber ^^ { str => VARB(str) } }
-  //     def tokens: Parser[List[WorkflowToken]] = {
-  //       phrase(poly) ^^ { rawTokens => }
-  //     }
-  //   }
-  // }
   test("LexerParser") {
-    val p: PolynomialLexer = new PolynomialLexer()
-    println(p.parse(p.polynomial, "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"))
-    println(p.parse(p.polynomial, "5 * X^0 + 4 * X^1 = 4 * X^0"))
-    println(p.parse(p.polynomial, "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0"))
-    println(p.parse(p.polynomial, "5 + 4 * X + X^2= X^2"))
-    println(p.parse(p.polynomial, "1+4*X-X^2=0"))
-    println(p.parse(p.polynomial, "9x^2+6x+3"))
+    PolynomialLexer("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0") match {
+      case Left(s) => println(s)
+      // (Some(-0.47513146390886934),Some(0.9052389907905898)) did not equal
+      // (Some(-0.4751314639088694), Some(0.9052389907905898))
+      case Right(p) => assert(p.solve == (Some(-0.47513146390886934),Some((2*sqrt(1030)+20)/93)))
+    }
+    PolynomialLexer("5 * X^0 + 4 * X^1 = 4 * X^0") match {
+      case Left(s) => println(s)
+      case Right(p) => assert(p.solve == (Some(-0.25), None))
+    }
+    PolynomialLexer("8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0") match {
+      case Left(s) => println(s)
+      case Right(p) => println(p.simplify.longString)
+    }
+    PolynomialLexer("2 * X^2 + 4 * X^1 - 6 * X^0 = 0 * X^0") match {
+      case Left(s) => println(s)
+      case Right(p) => assert(p.solve == (Some(1), Some(-3)))
+    }
+    // BONUS
+    // println(PolynomialLexer("5 + 4 * X + X^2= X^2"))
+    // println(PolynomialLexer("1+4*X-X^2=0"))
+    // println(PolynomialLexer("9x^2+6x+3"))
   }
 }
